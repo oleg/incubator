9c3a8087b5655d1198d48ace49128513da2bd99b
Oleg Prozorov <pro.oleg@gmail.com>
2020-11-26 21:31:46 +0100

    Wall width

--- books/mazes-in-go/render/imgpng.go
+++ books/mazes-in-go/render/imgpng.go
@@ -8,6 +8,8 @@ import (
 	"mazes/maze"
 )
 
+var ww = 4
+
 //todo refactor
 //todo pass options?
 func ToPng(grid *maze.Grid, wr io.Writer) error {
@@ -16,12 +18,12 @@ func ToPng(grid *maze.Grid, wr io.Writer) error {
 	img := image.NewRGBA(image.Rect(0, 0, w, h))
 
 	grid.EachRow(func(n int, row []*maze.Cell) {
-		v := n * 4
+		v := n * ww
 		if n == 0 {
 			renderTop(grid, row, v+0, img)
 		}
 		renderMiddle(grid, row, v, img)
-		renderBottom(grid, row, v+4, img)
+		renderBottom(grid, row, v+ww, img)
 	})
 
 	err := png.Encode(wr, img)
@@ -32,43 +34,41 @@ func ToPng(grid *maze.Grid, wr io.Writer) error {
 }
 
 func renderTop(grid *maze.Grid, row []*maze.Cell, n int, img *image.RGBA) {
-	c := color.Black
-	img.Set(0, n, c)
+	img.Set(0, n, color.Black)
 	for i, cell := range row {
-		y := i * 4
+		y := i * ww
 		if !cell.Linked(grid.North(cell)) {
-			img.Set(y+1, n, c)
-			img.Set(y+2, n, c)
-			img.Set(y+3, n, c)
+			img.Set(y+1, n, color.Black)
+			img.Set(y+2, n, color.Black)
+			img.Set(y+3, n, color.Black)
 		}
-		img.Set(y+4, n, c)
+		img.Set(y+ww, n, color.Black)
 	}
 }
 
 func renderBottom(grid *maze.Grid, row []*maze.Cell, n int, img *image.RGBA) {
 	img.Set(0, n, color.Black)
 	for i, cell := range row {
-		y := i * 4
+		y := i * ww
 		if !cell.Linked(grid.South(cell)) {
 			img.Set(y+1, n, color.Black)
 			img.Set(y+2, n, color.Black)
 			img.Set(y+3, n, color.Black)
 		}
-		img.Set(y+4, n, color.Black)
+		img.Set(y+ww, n, color.Black)
 	}
 }
 func renderMiddle(grid *maze.Grid, row []*maze.Cell, n int, img *image.RGBA) {
-	c := color.Black
-	img.Set(0, n+1, c)
-	img.Set(0, n+2, c)
-	img.Set(0, n+3, c)
+	img.Set(0, n+1, color.Black)
+	img.Set(0, n+2, color.Black)
+	img.Set(0, n+3, color.Black)
 
 	for i, cell := range row {
 		if !cell.Linked(grid.East(cell)) {
-			v := i*4 + 4
-			img.Set(v, n+1, c)
-			img.Set(v, n+2, c)
-			img.Set(v, n+3, c)
+			v := i*ww + ww
+			img.Set(v, n+1, color.Black)
+			img.Set(v, n+2, color.Black)
+			img.Set(v, n+3, color.Black)
 		}
 	}
 
