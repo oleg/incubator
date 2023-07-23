2ecaf3307a76bd182191bb5da1de42666a3403e6
Oleg Prozorov <oleg.entw@gmail.com>
2023-07-23 13:35:26 +0200

    rust: guessing game; final version

--- the-rust-programming-language/chapter2/guessing_game/src/main.rs
+++ the-rust-programming-language/chapter2/guessing_game/src/main.rs
@@ -7,7 +7,6 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -18,17 +17,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
-            Ordering::Equal => println!("You win!"),
             Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
