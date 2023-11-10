bded4005a3997952288959c45a475cb9c58dee32
Oleg Prozorov <oleg.entw@gmail.com>
2023-11-10 21:03:26 +0100

    rust; tests for echor

--- command-line-rust/echor/src/main.rs
+++ command-line-rust/echor/src/main.rs
@@ -5,20 +5,25 @@ fn main() {
         .version("0.1.0")
         .author("Oleg Prozorov <oleg.entw@gmail.com>")
         .about("Rust echo")
-        .arg(Arg::with_name("text")
-            .value_name("TEXT")
-            .help("Input text")
-            .required(true)
-            .min_values(1)
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
         )
-        .arg(Arg::with_name("omit_newline")
-            .short("n")
-            .help("Do not print newline")
-            .takes_value(false)
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
         )
         .get_matches();
 
-    let text = matches.values_of_lossy("text").unwrap();
+    let text = matches.values_of_lossy("text").unwrap().join(" ");
 
-    println!("{:#?}", text);
+    let omit_newline = matches.is_present("omit_newline");
+    let nl = if omit_newline { "" } else { "\n" };
+
+    print!("{}{}", text, nl);
 }
