4f50404b02694c124b467acbdc6ebf40e13e093c
Oleg Prozorov <oleg.entw@gmail.com>
2023-12-06 08:45:32 +0100

    aoc2023; day1 v2

--- aoc2023rust/src/main.rs
+++ aoc2023rust/src/main.rs
@@ -1,4 +1,6 @@
-use std::env;
+use io::Error;
+use io::ErrorKind::Other;
+use std::{env, io};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -8,41 +10,67 @@ fn main() {
     }
 }
 
+#[derive(Debug)]
+struct Matcher {
+    s: String,
+}
+
+impl Matcher {
+    fn new() -> Matcher {
+        Matcher { s: String::new() }
+    }
+    fn push(&mut self, c: char) {
+        self.s.push(c);
+    }
+    fn digit(&self) -> Option<i32> {
+        match self.s.as_str() {
+            "one" | "1" => Some(1),
+            "two" | "2" => Some(2),
+            "three" | "3" => Some(3),
+            "four" | "4" => Some(4),
+            "five" | "5" => Some(5),
+            "six" | "6" => Some(6),
+            "seven" | "7" => Some(7),
+            "eight" | "8" => Some(8),
+            "nine" | "9" => Some(9),
+            _ => None,
+        }
+    }
+    fn push_and_match_digit(&mut self, c: char) -> Option<i32> {
+        self.push(c);
+        self.digit()
+    }
+}
 
 fn run() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<_> = env::args().collect();
-    let filename = &args[1];
-
-    let file = File::open(filename)?;
+    let file = File::open(&args[1])?;
     let reader = BufReader::new(file);
-
 
     let mut sum = 0;
     for line in reader.lines() {
-        let mut nums: Vec<(Option<usize>, usize)> = Vec::new();
-        let line = line.unwrap();
-
-        for (i, v) in ["1", "2", "3", "4", "5", "6", "7", "8", "9"].iter().enumerate() {
-            nums.push((line.find(v), i + 1));
-            nums.push((line.rfind(v), i + 1));
+        let mut matchers: Vec<Matcher> = Vec::new();
+        let mut first: Option<i32> = None;
+        let mut last: Option<i32> = None;
+        for c in line?.chars() {
+            matchers.push(Matcher::new());
+            for m in &mut matchers {
+                if let Some(d) = m.push_and_match_digit(c) {
+                    if let None = first {
+                        first = Some(d);
+                    }
+                    last = Some(d);
+                }
+            }
         }
-        for (i, v) in ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"].iter().enumerate() {
-            nums.push((line.find(v), i + 1));
-            nums.push((line.rfind(v), i + 1));
+        match (first, last) {
+            (Some(f), Some(l)) => {
+                sum += f * 10 + l;
+            }
+            _ => Err(Box::new(Error::new(Other, "failed to get first and last digit")))?,
         }
-
-        let mut pos2val = nums.iter()
-            .filter(|(pos, _)| pos.is_some())
-            .map(|(pos, val)| (pos.unwrap(), val))
-            .collect::<Vec<_>>();
-        pos2val.sort_by(|(a, _), (b, _)| a.cmp(&b));
-
-        let d1 = pos2val.first().unwrap().1;
-        let d2 = pos2val.last().unwrap().1;
-        sum += d1 * 10 + d2;
     }
 
     println!("{}", sum);
-
     Ok(())
 }
