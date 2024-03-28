6edbf39dae231f885b6e626ecfd142505f08cb3c
Oleg Prozorov <oleg.entw@gmail.com>
2024-03-28 13:04:56 +0100

    algo; array 101; duplicate zero

--- go/algo/leet/array101.go
+++ go/algo/leet/array101.go
@@ -45,3 +45,64 @@ func findNumbers(nums []int) int {
 	}
 	return count
 }
+
+func duplicateZeros(arr []int) {
+	zeros := 0
+	lastIndex := len(arr) - 1
+	for left := 0; left <= (lastIndex - zeros); left++ {
+		if arr[left] == 0 {
+			if left == lastIndex-zeros {
+				arr[lastIndex] = 0
+				lastIndex--
+				break
+			}
+			zeros++
+		}
+	}
+	for i := lastIndex - zeros; i >= 0; i-- {
+		arr[i+zeros] = arr[i]
+		if arr[i] == 0 {
+			zeros--
+			arr[i+zeros] = 0
+		}
+	}
+}
+
+func duplicateZerosV1(arr []int) {
+	zeros := 0
+	for _, v := range arr {
+		if v == 0 {
+			zeros += 1
+		}
+	}
+	length := len(arr)
+	if zeros == 0 || zeros == length {
+		return
+	}
+
+	oldEnd := length - 1
+	newEnd := oldEnd + zeros
+
+	for newEnd >= 0 {
+		if newEnd < length {
+			arr[newEnd] = arr[oldEnd]
+		}
+		if arr[oldEnd] == 0 {
+			newEnd--
+			if newEnd < length {
+				arr[newEnd] = 0
+			}
+		}
+		newEnd--
+		oldEnd--
+	}
+}
+func countZeros(arr []int) int {
+	zeros := 0
+	for i := 0; i < len(arr) && (i+zeros) < len(arr); i++ {
+		if arr[i] == 0 {
+			zeros++
+		}
+	}
+	return zeros
+}
--- go/algo/leet/array101_test.go
+++ go/algo/leet/array101_test.go
@@ -2,6 +2,7 @@ package leet
 
 import (
 	"reflect"
+	"slices"
 	"testing"
 )
 
@@ -63,6 +64,72 @@ func Test_findNumbers(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := findNumbers(tt.nums); got != tt.want {
 				t.Errorf("findNumbers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_duplicateZeros(t *testing.T) {
+	tests := []struct {
+		name string
+		arr  []int
+		want []int
+	}{
+		{"a", []int{1, 0, 2}, []int{1, 0, 0}},
+		{"b", []int{1, 0, 0, 2}, []int{1, 0, 0, 0}},
+		{"empty", []int{}, []int{}},
+		{"zeros 1", []int{0}, []int{0}},
+		{"zeros 2", []int{0, 0}, []int{0, 0}},
+		{"zeros 3", []int{1, 0}, []int{1, 0}},
+		{"zeros 4", []int{1, 0, 0}, []int{1, 0, 0}},
+		{"zeros 5", []int{0, 0, 0}, []int{0, 0, 0}},
+		{"zeros 6", []int{1, 0, 0, 0}, []int{1, 0, 0, 0}},
+		{"no zeros", []int{1, 2, 3}, []int{1, 2, 3}},
+		{"with zeros", []int{1, 0, 3, 4}, []int{1, 0, 0, 3}},
+		{"with two zeros", []int{1, 0, 0, 4, 5, 6}, []int{1, 0, 0, 0, 0, 4}},
+		{"with zeros at start", []int{0, 1, 2}, []int{0, 0, 1}},
+		{"example 1", []int{1, 0, 2, 3, 0, 4, 5, 0}, []int{1, 0, 0, 2, 3, 0, 0, 4}},
+		{"many zeros", []int{0, 0, 0, 0, 0, 0, 0}, []int{0, 0, 0, 0, 0, 0, 0}},
+		{"example 2", []int{8, 4, 5, 0, 0, 0, 0, 7}, []int{8, 4, 5, 0, 0, 0, 0, 0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			duplicateZeros(tt.arr)
+			if !slices.Equal(tt.arr, tt.want) {
+				t.Errorf("duplicateZeros() = %v, want %v", tt.arr, tt.want)
+			}
+
+		})
+	}
+}
+func Test_countZeros(t *testing.T) {
+	tests := []struct {
+		name string
+		arr  []int
+		want int
+	}{
+		{"a", []int{1, 0, 2}, 1},
+		{"b", []int{1, 0, 0, 2}, 2},
+		{"c", []int{1, 2, 0, 0}, 1},
+		{"empty", []int{}, 0},
+		{"zeros 1", []int{0}, 1},
+		{"zeros 2", []int{0, 0}, 1},
+		{"zeros 3", []int{1, 0}, 1},
+		{"zeros 4", []int{1, 0, 0}, 1},
+		{"zeros 5", []int{0, 0, 0}, 2},
+		{"zeros 6", []int{1, 0, 0, 0}, 2},
+		{"no zeros", []int{1, 2, 3}, 0},
+		{"with zeros", []int{1, 0, 3, 4}, 1},
+		{"with two zeros", []int{1, 0, 0, 4, 5, 6}, 2},
+		{"with zeros at start", []int{0, 1, 2}, 1},
+		{"example 1", []int{1, 0, 2, 3, 0, 4, 5, 0}, 2},
+		{"many zeros", []int{0, 0, 0, 0, 0, 0, 0}, 4},
+		{"example 2", []int{8, 4, 5, 0, 0, 0, 0, 7}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countZeros(tt.arr); got != tt.want {
+				t.Errorf("countZeros() = %v, want %v", got, tt.want)
 			}
 		})
 	}
