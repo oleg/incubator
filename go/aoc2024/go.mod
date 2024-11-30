60567c918e6772a4d37c87fc3e064b6d3613a779
Oleg Prozorov <oleg.entw@gmail.com>
2024-11-30 19:56:47 +0100

    aoc2024; prepare for day1

---
+++ go/aoc2024/go.mod
@@ -0,0 +1,3 @@
+module github.com/oleg/incubator/aoc2024
+
+go 1.23.3
---
+++ go/aoc2024/misc/misc.go
@@ -0,0 +1,41 @@
+package misc
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+)
+
+func MustOpen(filename string) io.Reader {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	return file
+}
+
+func MustReadFileToString(filename string) string {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
+}
+
+func MustReadInts(reader io.Reader) []int {
+	ints := make([]int, 0, 10)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		ints = append(ints, MustAtoi(scanner.Text()))
+	}
+	return ints
+}
+
+func MustAtoi(str string) int {
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return num
+}
